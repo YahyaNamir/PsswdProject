6ab50705493af900191022257faace10fed7977a
YAHYA NAMIR <132499945+YahyaNamir@users.noreply.github.com>
2024-05-24 16:46:07 +0100

    rel
    rel

--- front-psswd/src/pages/Details_D.jsx
+++ front-psswd/src/pages/Details_D.jsx
@@ -13,7 +13,8 @@ const Details_D = () => {
   return (
     <div className="p-8 bg-gray-50 min-h-screen">
       <h1 className="text-4xl font-extrabold text-gray-900 mb-6 text-center">
-        DÉTAILS DU DOMAINE
+        DÉTAILS DU DOMAINE 
+        
       </h1>
       <div className="bg-white shadow-lg rounded-lg p-6">
         <h2 className="text-3xl font-bold text-gray-800 mb-4">
