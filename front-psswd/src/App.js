7f78c384fdcfb72f004d4d41e501d978886ac095
YAHYA NAMIR <132499945+YahyaNamir@users.noreply.github.com>
2024-07-16 18:29:31 +0100

    Remove
    Remove more

--- front-psswd/src/App.js
+++ front-psswd/src/App.js
@@ -36,8 +36,6 @@ function App() {
     { id: 1, name: "Project A" },
     { id: 2, name: "Project B" },
     { id: 3, name: "Project C" },
-    { id: 4, name: "Project D" },
-    { id: 5, name: "Project E" },
   ];
 
   return (
